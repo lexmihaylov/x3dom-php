88481f4235a43b73685da1ce2243275f756f54c0
Alexander Mihaylov <lex.mihaylov@gmail.com>
2013-05-17 13:06:52 +0300

    updated blender shell wrapper

--- blender/Shell.php
+++ blender/Shell.php
@@ -14,9 +14,11 @@ require __DIR__.'/Config.php';
 
 class Shell {
     private $x3dInstance;
+    private $shellCommand = array();
     
     public function __construct($x3dInstance) {
         $this->x3dInstance = $x3dInstance;
+        
     }
     
     public function __destruct() {
@@ -30,34 +32,51 @@ class Shell {
         $this->x3dInstance->saveX3D(__DIR__.'/tmp.x3d');
     }
     
-    private function importCommandLine() {
-        $scriptDir = Config::$scriptIncludeDir;
-        $executable = Config::$blenderPath;
-        $importScript = Config::$importScript;
-        $tmpFile = __DIR__.'/tmp.x3d';
-        $blankStage = __DIR__.'/stage.blend';
-        
-        return "SCRIPT_DIR=\"{$scriptDir}\" TMP_FILE=\"{$tmpFile}\" {$executable} {$blankStage} -P {$importScript}";
+    private function runInBackgroun() {
+        $this->shellCommand[] = '-b';
     }
     
-    private function exportImage($filename) {
+    private function setExecutableCommand() {
         $scriptDir = Config::$scriptIncludeDir;
         $executable = Config::$blenderPath;
-        $importScript = Config::$importScript;
         $tmpFile = __DIR__.'/tmp.x3d';
+        
+        $this->shellCommand[] = "SCRIPT_DIR=\"{$scriptDir}\" TMP_FILE=\"{$tmpFile}\" {$executable}";
+    }
+    
+    private function prepareImportCommand() {
+        $importScript = Config::$importScript;
         $blankStage = __DIR__.'/stage.blend';
         
-        return "SCRIPT_DIR=\"{$scriptDir}\" TMP_FILE=\"{$tmpFile}\" {$executable} -b {$blankStage} -P {$importScript} -o {$filename} -F PNG -x 1 -f 1";
+        $this->shellCommand[] = "{$blankStage} -P {$importScript}";
+    }
+    
+    private function setRenderedImageOptions($filename) {
+        $this->shellCommand[] = "-o {$filename} -F PNG -x 1 -f 1";
+    }
+    
+    private function execCommand() {
+        system(implode(' ', $this->shellCommand));
     }
     
     public function import() {
         $this->saveX3D();
-        system($this->importCommandLine());
+        
+        $this->setExecutableCommand();
+        $this->prepareImportCommand();
+        
+        $this->execCommand();
     }
     
     public function saveImage($filename) {
         $this->saveX3D();
-        system($this->exportImage($filename));
+        
+        $this->setExecutableCommand();
+        $this->runInBackgroun();
+        $this->prepareImportCommand();
+        $this->setRenderedImageOptions($filename);
+        
+        $this->execCommand();
     }
 }
 
